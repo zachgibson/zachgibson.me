7cd3fa01a5c80e0145275413482ba285ee14d9a7
Zachary Gibson <zackgib@aol.com>
2019-11-26 14:40:26 -0600

    Remove all previous design and tooling, add blank fresh NextJS

--- src/pages/experiments.tsx
+++
@@ -1,5 +0,0 @@
-import * as React from "react"
-
-const ExperimentsPage = () => <h1>Experiments page 🥶</h1>
-
-export default ExperimentsPage
--- src/pages/work.tsx
+++
@@ -1,5 +0,0 @@
-import * as React from "react"
-
-const WorkPage = () => <h1>Work page 🥶</h1>
-
-export default WorkPage
